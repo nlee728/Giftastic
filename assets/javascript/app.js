ab9dec64388994094999737ec208ea00789825eb
unknown <38478896+nlee728@users.noreply.github.com>
2018-07-13 23:37:46 -0400

    correctly shows 10 gifs when button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
  // Initial array of emotions
- var emotions = ["happy", "awkward", "sad", "crazy"];
+ var emotions = ["cool", "awkward", "sad", "excited"];
 
  // displayemotionInfo function re-renders the HTML to display the appropriate content
  function displayemotionInfo() {
 
    var emotion = $(this).attr("data-name");
-   var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + emotion;
+   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        emotion + "&api_key=dc6zaTOxFJmzC&limit=10";
 
    // Creating an AJAX call for the specific emotion button being clicked
    $.ajax({
@@ -15,67 +16,80 @@
 
     console.log(response);
 
-     // Creating a div to hold the emotion
+    // storing the data from the AJAX request in the results variable
+    var results = response.data;
+
+    // Looping through each result item
+    for (var i = 0; i < results.length; i++) {
+
+    // Retrieving the URL for the image and adding image to div
      var emotionDiv = $("<div class='emotion'>");
 
-     // Storing the rating data
-     var rating = response.data.rating;
+     var gifURL = results[i].images.downsized.url;
+     var stillURL = results[i].images.downsized_still.url;
 
-     // Creating an element to have the rating displayed
-     var p = $("<p>").text("Rating: " + rating);
-
-     // Displaying the rating
-     emotionDiv.append(p);
-
-     // Retrieving the URL for the image
-     var imgURL = response.data.image_original_url;
-
-     // Creating an element to hold the image
-     var image = $("<img>").attr("src", imgURL);
-
-     // Appending the image
+     var image = $("<img>").attr("src", stillURL)
+                           .attr("data-still", stillURL)
+                           .attr("data-animate", gifURL)
+                           .attr("data-state", "still")
+                           .attr("class", "gif");
      emotionDiv.append(image);
 
-     // Putting the entire gif above the previous gifs
+     //Get rating info and append to div
+      var rating = response.data[i].rating;
+      var p = $("<p>").text("Rating: " + rating);
+      emotionDiv.append(p);
      $("#emotions-view").prepend(emotionDiv);
+
+    }
    });
 
  }
 
+//Function that handles clicks on the gif images to switch between still and animated
+ $(".gif").on("click", function() {
+     console.log("click");
+    var state = $(this).attr("data-state");
+    console.log(state);
+
+    //If still, switch to animated gif and vice-versa
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+    console.log(state);
+  });
+
+
  // Function for displaying emotion data
  function renderButtons() {
 
-   // Deleting the emotions prior to adding new emotions
-   // (this is necessary otherwise you will have repeat buttons)
+   // Deleting the emotions prior to adding new emotions to avoid repeat buttons
    $("#buttons-view").empty();
 
    // Looping through the array of emotions
    for (var i = 0; i < emotions.length; i++) {
 
-     // Then dynamicaly generating buttons for each emotion in the array
-     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+     // Dynamically generating buttons for each emotion in the array
      var a = $("<button>");
-     // Adding a class of emotion-btn to our button
      a.addClass("emotion-btn");
-     // Adding a data-attribute
      a.attr("data-name", emotions[i]);
-     // Providing the initial button text
      a.text(emotions[i]);
-     // Adding the button to the buttons-view div
      $("#buttons-view").append(a);
    }
  }
 
- // This function handles events where a emotion button is clicked
+ // Click event handler for the emotions buttons
  $("#add-emotion").on("click", function(event) {
    event.preventDefault();
-   // This line grabs the input from the textbox
+   // Grabs the input from the textbox and adds to array
    var emotion = $("#emotion-input").val().trim();
-
-   // Adding emotion from the textbox to our array
    emotions.push(emotion);
 
-   // Calling renderButtons which handles the processing of our emotion array
+   // Calling renderButtons to display the emotions array
    renderButtons();
  });
 
